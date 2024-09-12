f7c33eb5c95bb4570d1e86649a558dbc53cfab44
Lucas <lucasarieta@gmail.com>
2024-09-12 13:18:36 -0300

    feat: adicionado handler ao logar em sua conta e removido arquivos out

--- game/src/edu/pucrs/br/ui/UI.java
+++ game/src/edu/pucrs/br/ui/UI.java
@@ -44,18 +44,74 @@ public class UI {
         } catch (Exception e) {
             System.out.println("Erro ao ler a opção: " + e.getMessage());
         }
+    }
 
+    private void onLogin() {
+        // TODO: Mostrar todas as propostas de troca pendentes para o jogador logado (implementação: Eduardo Rosa)
+
+        this.showOptionsMenu();
+    }
+
+    private void showOptionsMenu() {
+        System.out.println("[===================]");
+        System.out.println("1 - [Admin] Cadastrar novo item");
+        System.out.println("2 - [Admin] Listar os itens de todos os jogadores, ordenado por valor");
+        System.out.println("3 - [Player] Listar meus itens, ordenado alfabeticamente");
+        System.out.println("4 - [Player] Buscar itens por termo (parte do nome, descrição ou categoria)");
+        System.out.println("5 - [Player] Propor troca de itens para outro jogador");
+        System.out.println("6 - [Player] Mostrar minhas propostas de troca pendentes");
+        System.out.println("7 - [Player] Detalhes sobre proposta de troca");
+        System.out.println("8 - [Admin] Estatísticas do jogo");
+        System.out.println("[===================]");
+        System.out.print("Digite a opcao desejada: ");
+
+        try {
+            do {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println("Pendente de implementação: Iuri Queiroz");
+                        break;
+                    case 2:
+                        System.out.println("Pendente de implementação: Bernardo Medeiros");
+                        break;
+                    case 3:
+                        System.out.println("Pendente de implementação: Felipe Rambor");
+                        break;
+                    case 4:
+                        System.out.println("Pendente de implementação: Luiza Mitchell");
+                        break;
+                    case 5:
+                        System.out.println("Pendente de implementação: Bernardo Kirsch");
+                        break;
+                    case 6:
+                        System.out.println("Pendente de implementação: Eduardo Rosa");
+                        break;
+                    case 7:
+                        System.out.println("Pendente de implementação: Pedro Petrini | Aceite ou recusa de troca: Erick Carpes");
+                        break;
+                    case 8:
+                        System.out.println("Pendente de implementação: Lucas Arieta");
+                        break;
+
+                    default: System.out.println("Opcao invalida. Redigite.");
+                }
+            } while (true);
+        } catch (Exception e) {
+            System.out.println("Erro ao ler a opção: " + e.getMessage());
+        }
     }
 
     private void login() {
         System.out.println("Digite o seu email: ");
         String email = this.scanner.next();
-        System.out.println("Digite seu PIN: ");
+        System.out.println("Digite o seu PIN: ");
         String pin = this.scanner.next();
 
         boolean result = this.players.login(email,pin);
         if(result){
             System.out.println("Login efetuado com sucesso!");
+            this.onLogin();
         }else{
             System.out.println("Erro ao fazer login!");
             this.showAuthMenu();
